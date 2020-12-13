bb2f51a9554e4ae90dd5e87cfdbca3537f0f9764
doob009 <kimphong90@hotmail.com>
2020-12-13 14:35:45 -0600

    Add Mouse Callback

--- opencv-ClickEvent.py
+++ opencv-ClickEvent.py
@@ -3,6 +3,10 @@ import numpy as np
 
 cam = cv.VideoCapture(0)
 
+def callback(event,x,y,flags,params):
+    if event == cv.EVENT_LBUTTONDOWN:
+        print('the event is: ' ,event)
+
 while True:
     resl , frame = cam.read()
     if resl == False:
@@ -10,6 +14,8 @@ while True:
         break
     cv.namedWindow('S3-Cam',cv.WINDOW_NORMAL)
     cv.resizeWindow('S3-Cam', 800,600)
+    cv.setMouseCallback('S3-Cam',callback)
+
     cv.imshow('S3-Cam',frame)
     if cv.waitKey(1) == ord('q'):
         break

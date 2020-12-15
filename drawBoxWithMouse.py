79eb0e76836a6e66f1727452ae311380cae3ede6
doob009 <kimphong90@hotmail.com>
2020-12-15 12:32:18 -0600

    Better Version with moving the Box with buttons

--- drawBoxWithMouse.py
+++ drawBoxWithMouse.py
@@ -3,20 +3,24 @@ import cv2 as cv
 import numpy as np
 import math
 
-st_point = ()
-ed_point = ()
-dx = 10
-dy = 10
+draw = False
+
+dx = 30
+dy = 30
 
 # callback function for Mouse Events
 def callback(event,x,y,flags,params):
-    global st_point
-    global ed_point 
+    #Define varibale for local and set as global to be accessible by other function
+    global x1,y1,x2,y2
+    global draw 
     if event == cv.EVENT_LBUTTONDOWN:
-        ed_point =()
-        st_point = (x,y)
+        x1 = x
+        y1 = y
+        draw = False # reset the value when draw another rectangle
     if event == cv.EVENT_LBUTTONUP:
-        ed_point =(x,y)
+        x2 = x
+        y2 = y
+        draw = True 
 
 # def cal_distance(point1,point2):
 #     rel = math.sqrt(  (point2[0] - point1[0])**2 + (point2[1] - point1[1])**2  )
@@ -26,7 +30,7 @@ def callback(event,x,y,flags,params):
 ''' 
 Capture the video
 Create window with title
-Set mousr event listener to the window
+Set mouse event listener to the window
 '''
 cam = cv.VideoCapture(0)
 cv.namedWindow('LG-Cam',cv.WINDOW_AUTOSIZE)
@@ -48,24 +52,29 @@ while True:
 
     # Create A condition - True or False to go to another path
     # OR do change the conditon of global variable to void this path
-    # ONLY Create a rectangle if there are 2 points
-    if  st_point and ed_point: 
+    # ONLY Create a rectangle if there are 2 points or sth True
+    # not -- to check empty variable
+    if draw and  x1 != x2 :
         
-        cv.rectangle(resized_frame, st_point, ed_point, (0,255,0), 3)
-        # Create Region Of Interest
-        roi = resized_frame[st_point[1]: ed_point[1], st_point[0]: ed_point[0]]
+        cv.rectangle(resized_frame, (x1,y1), (x2,y2), (0,255,0) , 3)
+        roi = resized_frame[y1:y2, x1:x2]
         cv.imshow('ROI',roi)
-        # Moving the box by press a key -> Problem: typle is immutable
-        # if keyEvent == ord('d'):
-        #     print('d pressed')
-        #     st_point[0] += 50
-        #     ed_point[0] += 50
-        #     print(st_point)
+
+    
     cv.imshow('LG-Cam',resized_frame)
     keyEvent = cv.waitKey(1)
     if keyEvent == ord('q'):
         break
-    
-    
+    if keyEvent == ord('d'):
+        x1 += dx
+        x2 += dx
+        if x2 >= new_width:
+            pass
+            x2 = new_width  
+    if keyEvent == ord('a'):
+        x1 -= dx
+        x2 -= dx
+        if x1 < 0:
+            pass
 cam.release()
 cv.destroyAllWindows()

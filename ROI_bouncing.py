d187410ebc44b49bb5d10cde97686e3494626be7
doob009 <kimphong90@hotmail.com>
2020-12-14 18:53:42 -0600

    Add ROI bouncing

--- ROI_bouncing.py
+++ ROI_bouncing.py
@@ -7,11 +7,9 @@ cv.namedWindow('LG-Cam',cv.WINDOW_AUTOSIZE)
 x_start = 50
 y_start = 50
 box_ratio = 0.15
-dx = 50
-dy = 20
+dx = 1
+dy = 2
 
-
-print('x_start is :',x_start)
 while True:
     resl, frame = cam.read()
     if resl == False:
@@ -28,8 +26,17 @@ while True:
     #Boucing Box
     b_w = int(box_ratio * new_width) #define width of the box
     b_h = int(box_ratio * new_height) # define the height of the box
-
-    cv.rectangle(resized_frame,(x_start,y_start),(x_start + b_w, y_start + b_h),(0,255,0),3 )
+    ''' 
+    copy the color region
+    convert frame into gray
+    convert frame back into color to get the right shape = color image shape
+    assign color region -> gray region
+    '''    
+    roi = resized_frame [ y_start: y_start + b_h,  x_start: x_start + b_w].copy() 
+    gray_frame = cv.cvtColor(resized_frame, cv.COLOR_BGR2GRAY)
+    fake_color = cv.cvtColor(gray_frame, cv.COLOR_GRAY2BGR)
+    fake_color[ y_start: y_start + b_h, x_start:x_start + b_w] = roi
+    cv.rectangle(fake_color,(x_start,y_start),(x_start + b_w, y_start + b_h), (0,255,0), 3)
     x_start = x_start + dx
     y_start = y_start + dy
     if x_start <= 0 or x_start + b_w > new_width :
@@ -37,7 +44,7 @@ while True:
     if  y_start <= 0 or y_start +b_h >= new_height :
         dy = -1 * dy
         
-    cv.imshow('LG-Cam', resized_frame)
+    cv.imshow('LG-Cam', fake_color)
     keyEvent = cv.waitKey(1)
     if keyEvent == ord('q'):
         break

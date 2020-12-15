328133eb25f41e647f6568c2bead56b02b2c5f00
doob009 <kimphong90@hotmail.com>
2020-12-15 11:01:13 -0600

    Drag Mouse to create Region of INterest

--- drawBoxWithMouse.py
+++ drawBoxWithMouse.py
@@ -1,13 +1,13 @@
 
 import cv2 as cv 
 import numpy as np
+import math
 
 st_point = ()
 ed_point = ()
 
-
+# callback function for Mouse Events
 def callback(event,x,y,flags,params):
-    
     global st_point
     global ed_point 
     if event == cv.EVENT_LBUTTONDOWN:
@@ -15,11 +15,24 @@ def callback(event,x,y,flags,params):
         st_point = (x,y)
     if event == cv.EVENT_LBUTTONUP:
         ed_point =(x,y)
-        
 
+# def cal_distance(point1,point2):
+#     rel = math.sqrt(  (point2[0] - point1[0])**2 + (point2[1] - point1[1])**2  )
+#     return rel
+
+
+''' 
+Capture the video
+Create window with title
+Set mousr event listener to the window
+'''
 cam = cv.VideoCapture(0)
 cv.namedWindow('LG-Cam',cv.WINDOW_AUTOSIZE)
 cv.setMouseCallback('LG-Cam',callback)
+
+col =0
+row =0 
+#Show the video
 while True:
     resl , frame = cam.read()
     if resl == False:
@@ -38,11 +51,13 @@ while True:
     # ONLY Create a rectangle if there are 2 points
     if  st_point and ed_point: 
         cv.rectangle(resized_frame, st_point, ed_point, (0,255,0), 3)
+        # Create Region Of Interest
+        roi = resized_frame[st_point[1]: ed_point[1], st_point[0]: ed_point[0]]
+        cv.imshow('ROI',roi)
     
-    # 
     cv.imshow('LG-Cam',resized_frame)
     if cv.waitKey(1) == ord('q'):
         break
-
+    
 cam.release()
 cv.destroyAllWindows()
